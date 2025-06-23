90e6fc41cbd6c42074eb61e21b55de0189c7b19b
Manas Ranjan Biswal <83444633+Manas8260@users.noreply.github.com>
2025-06-23 13:45:56 +0530

    end to end

--- streamlit_reg.py
+++ streamlit_reg.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import pandas as pd
 import pickle
 
-model = load_model('model.h5')
+model = load_model('regression_model.h5')
 
 #loading encdoers and scalers 
 with open('onehot_encoder_geo.pkl','rb') as file:
@@ -18,7 +18,7 @@ with open('scaler.pkl','rb') as file:
     scaler = pickle.load(file)
 
 #strimlit app
-st.title("Costumer Churn Prediction")
+st.title("Salary Prediction")
 
 #user input 
 geography = st.selectbox('Geography',en_geo.categories_[0]) 
@@ -26,7 +26,7 @@ gender = st.selectbox('Gender',en_gender.classes_)
 age = st.slider('Age',18,92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('credit_score')
-estimated_salary = st.number_input('estimated_salary')
+exited = st.selectbox('Exited',[0,1])
 tenure = st.slider('Tenure',0,10)
 num_of_products = st.slider('Nmumber of products',1,4)
 has_cr_card = st.selectbox('Has credit card',[0,1])
@@ -41,7 +41,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited':exited
 })
 
 # One-hot encode 'Geography'
@@ -57,11 +57,6 @@ input_data_scaled = scaler.transform(input_data)
 
 # Predict churn
 prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
 
-st.write(f'Churn Probability: {prediction_proba:.2f}')
+st.write(f'Estimated Salary: {prediction}')
 
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.') 
